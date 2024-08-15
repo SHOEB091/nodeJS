10bdb69d2b7e50c805081eca3aa37200242d31da
shoeb <wiqbal091@gmail.com>
2024-08-15 17:35:02 +0530

    Blog project home / signin / signup Implemented

--- BlogProject/index.js
+++ BlogProject/index.js
@@ -1,7 +1,16 @@
 const express= require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 8000;
+const userRoute  = require('./routes/user')
+
+mongoose
+    .connect('mongodb://localhost:27017/blogify')
+    .then((e) => {
+        console.log("Connected to the database");
+    })
 
 
 app.set("view engine","ejs");
@@ -13,7 +22,8 @@ app.use(express.urlencoded({extended:false}));//it is used to handle form data
 app.get('/', (req, res) => {
     res.render("home");
   })
+app.use('/user',userRoute);
   
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
